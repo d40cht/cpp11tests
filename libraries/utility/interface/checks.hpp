4a013ed661b4307f3dc080be05bf5be26c4ab9e3
Alex Wilson <alex.wilson@navetas.com>
2013-03-25 13:04:35 +0000

    Missing header.

---
+++ libraries/utility/interface/checks.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+
+template<typename T>
+inline void check_equal( const char* FILE, int line, const T& lhs, const T& rhs )
+{
+    if ( lhs != rhs )
+    {
+        std::cerr << "Assertion Failure: (" << FILE << ", " << line << "): " << lhs << " != " << rhs << std::endl;
+    }
+}
+
+#define CHECK_EQUAL( lhs, rhs ) check_equal( __FILE__, __LINE__, (lhs), (rhs) )
+
+class AssertionFailureException : public std::exception
+{
+public:
+    AssertionFailureException( const char* message ) : m_message(message)
+    {
+    }
+    
+private:
+    const char* m_message;
+};
+
+inline void throwing_assert( bool predicate, const char* message )
+{
+    if ( !predicate ) throw AssertionFailureException( message );
+}
+
+
