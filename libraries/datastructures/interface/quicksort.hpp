71ce3f0c54f81d2588fd5a7677108f703f5de09c
Alex Wilson <alex.wilson@navetas.com>
2013-03-25 16:43:25 +0000

    Missing file.

---
+++ libraries/datastructures/interface/quicksort.hpp
@@ -0,0 +1,15 @@
+
+
+template<typename T>
+void quickSort( std::vector<T>& data )
+{
+    int length = data.size();
+    std::vector<T> buf( data.begin(), data.end() );
+    
+    auto pass = [length]( int from, int to, const std::vector<T>& from, std::vector<T>& to ) -> void
+    {
+    }
+    
+}
+
+
